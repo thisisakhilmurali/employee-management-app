dc02a3e9ba9d55eaa0eb94ca8a4676c1a3d10b06
Akhil Murali <63278163+akhilmuraliai@users.noreply.github.com>
2023-06-25 17:51:11 +0530

    Added Angular Code

---
+++ frontend/src/app/employee.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private baseURL = "http://localhost:8080/api/v1"
+
+  constructor(private httpClient: HttpClient) { }
+
+  getEmployeeList(): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(`${this.baseURL}/employees`);
+  }
+
+  createEmployee(employee: Employee): Observable<Object> {
+    return this.httpClient.post(`${this.baseURL}/add`, employee);
+  }
+  
+}
---
+++ frontend/src/app/employee.ts
@@ -0,0 +1,15 @@
+export class Employee {
+
+    id: number;
+    firstName: string;
+    lastName: string;
+    emailId: string;
+
+    constructor() {
+        this.id = 0;
+        this.firstName = '';
+        this.lastName = '';
+        this.emailId = '';
+    }
+
+}
